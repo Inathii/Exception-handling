9a0a781adf57927d60529b32f3d24b8a301d135e
A252426 <A252426@LSBSA-BC86HW3>
2023-12-07 09:09:53 +0200

    Add project files.

---
+++ Exception handling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enetr an number ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter another number ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num2 / num2);
+            }
+            catch (Exception e)
+            { //can also have one generic error message, or catch the exact error.
+                Console.WriteLine(e.Message);
+                //freeze
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
+            
+            
+    
+
